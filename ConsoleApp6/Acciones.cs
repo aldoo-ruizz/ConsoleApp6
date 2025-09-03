60d42a43099cca3b4c8821cdad2d35401d69ebe9
Minilab-ing04 <Minilab-ing04@MINILAB-ING04>
2025-09-03 08:21:06 -0600

    Agregar archivos de proyecto.

---
+++ ConsoleApp6/Acciones.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    internal class Acciones
+    {
+        private double[] numeros;
+        
+        public void Agregar()
+        {
+            int numelementos=Convert.ToInt32(Console.ReadLine());  
+            numeros = new double[numelementos];
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                Console.WriteLine("Numeros");
+                numeros[i]=Convert.ToDouble(Console.ReadLine());
+
+            }
+        }
+        public void Mostrar()
+        {
+            foreach (var i in numeros)
+            {
+                Console.WriteLine($"Numeros: {i}");
+            }
+        }
+        public void sumar()
+        {
+            return ;
+        }
+        public void promedio()
+        {
+
+        }
+    }
+}
---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ARREGLOS
+{
+    internal class Program
+    {
+        public enum men
+        { 
+            agregar, mostrar=1, sumar, promedio, min, max, primero, ultimo, ordenar, revez, nelementos, salir
+        }
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                switch (menu())
+                {
+                    case men.agregar:
+
+                        break;
+                    case men.mostrar:
+                        break;
+                    case men.sumar:
+                        break;
+                    case men.promedio:
+                        break;
+                    case men.min:
+                        break;
+                    case men.max:
+                        break;
+                    case men.primero:
+                        break;
+                    case men.ultimo:
+                        break;
+                    case men.ordenar:
+                        break;
+                    case men.revez:
+                        break;
+                    case men.nelementos:
+                        break;
+                    case men.salir:
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+        static men menu()
+        {
+            Console.WriteLine("1)Agragar");
+            Console.WriteLine("2)Mostrar");
+            Console.WriteLine("3)suma");
+            Console.WriteLine("4)promedio");
+            Console.WriteLine("5)Minimo");
+            Console.WriteLine("6)Maximo");
+            Console.WriteLine("7)Primero");
+            Console.WriteLine("8)ultimo");
+            Console.WriteLine("9)ordenar");
+            Console.WriteLine("10)al revez");
+            Console.WriteLine("11)Numero de elementos");
+            Console.WriteLine("12)Salir");
+            men opc= (men)Convert.ToInt32(Console.ReadLine());
+            return opc;
+        }
+    }
+}
